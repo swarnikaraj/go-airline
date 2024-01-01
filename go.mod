4d49edac71ad8b62311b99011dbfcb60118ccf78
swarnnikarajsingh <swarnnika.singh@teganalytics.com>
2024-01-01 13:13:52 +0530

    base code with a print

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ticket-booking-app
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var applicationName = "Go-Airline"
+	const totalTicket = 500
+	fmt.Printf("Welcome to the %v ticker booking", applicationName)
+
+}
